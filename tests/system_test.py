e0cc5f4d56eb9194c5f3d652bfac146898d5de9f
Philipp Weißmann <mail@philipp-weissmann.de>
2016-01-01 19:46:43 +0100

    Try to use python3.x

--- tests/system_test.py
+++ tests/system_test.py
@@ -1,5 +1,8 @@
-import pytest
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
 import subprocess
+
 
 def caller(directory, command):
     proc=subprocess.Popen('../../../pwgrep/pwgrep.py {}'.format(command),
@@ -20,60 +23,45 @@ def caller(directory, command):
 #  128 + X -- Killed by signal X
 
 
+def helper_test_match(directory, command, stdout_shall, stderr_shall, return_code_shall):
+    return_code_is, stdout_is, stderr_is = caller(directory, command)
+    assert stderr_is.decode("utf-8") == (stderr_shall)
+    assert stdout_is.decode("utf-8") == (stdout_shall)
+    assert return_code_is == (return_code_shall)
+
+
 # Basic Search
 def test_text_match():
-    """ggrep Zen *"""
-    code, result, err = caller('simple', 'Zen *')
-    assert err == ''
-    assert result == 'zen_of_python.txt:The Zen of Python, by Tim Peters\n'
-    assert code == 0
+    helper_test_match('simple', 'Zen *', 'zen_of_python.txt:The Zen of '
+                                         'Python, by Tim Peters\n', '', 0)
 
 
 def test_binary_match():
-    """ggrep Hello *"""
-    code, result, err = caller('simple', 'Hello *')
-    assert err == ''
-    assert result == 'Binary file helloworld matches\n'
-    assert code == 0
+    helper_test_match('simple', 'Hello *', 'Binary file helloworld '
+                                           'matches\n', '', 0)
 
 
 def test_no_match():
-    """ggrep Hello *"""
-    code, result, err = caller('simple', 'ThisIsNeverFound *')
-    assert err == ''
-    assert result == ''
-    assert code == 1
+    helper_test_match('simple', 'ThisIsNeverFound *', '', '', 1)
 
 
 # No display of filename
 def test_text_match_no_filename():
-    """ggrep -h Zen *"""
-    code, result, err = caller('simple', '-h Zen *')
-    assert err == ''
-    assert result == 'The Zen of Python, by Tim Peters\n'
-    assert code == 0
+    helper_test_match('simple', '-h Zen *', 'The Zen of Python, by Tim '
+                                            'Peters\n', '', 0)
 
 
 def test_binary_match_no_filename():
-    """ggrep -h Hello *"""
-    code, result, err = caller('simple', '-h Hello *')
-    assert err == ''
-    assert result == 'Binary file helloworld matches\n'
-    assert code == 0
+    helper_test_match('simple', '-h Hello *', 'Binary file helloworld '
+                                              'matches\n', '', 0)
 
 
 # Ignore case
 def test_text_match_ignore_case():
-    """ggrep -i zEN *"""
-    code, result, err = caller('simple', '-i zEN *')
-    assert err == ''
-    assert result == 'zen_of_python.txt:The Zen of Python, by Tim Peters\n'
-    assert code == 0
+    helper_test_match('simple', '-i zEN *', 'zen_of_python.txt:The Zen of '
+                                            'Python, by Tim Peters\n', '', 0)
 
 
 def test_binary_match_ignore_case():
-    """ggrep -h Hello *"""
-    code, result, err = caller('simple', '-i hElLo *')
-    assert err == ''
-    assert result == 'Binary file helloworld matches\n'
-    assert code == 0
+    helper_test_match('simple', '-i hElLo *', 'Binary file helloworld '
+                                              'matches\n', '', 0)
