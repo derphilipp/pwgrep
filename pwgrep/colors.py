466cc201cedac257036b84afb20c06b0715bc821
Philipp Weißmann <mail@philipp-weissmann.de>
2016-01-04 11:46:24 +0100

    Documentation

--- pwgrep/colors.py
+++ pwgrep/colors.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 
-class bcolors:
+class ConsoleColors(object):
     BOLD = '\033[1m'
     CYAN = '\033[96m'
     RED = '\033[91m'
     ENDC = '\033[0m'
+    FILE = CYAN
     MATCH = BOLD + RED
